0cd9860ed486fbe4625bab85913c3948a0ad360a
Sarah <yamashige.sarah@gmail.com>
2018-06-07 20:31:24 -1000

    It works for some reason...

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,13 +1,11 @@
 module.exports = function acceptableSequence(str) {
   // write your code here
 
-for (var i = 0; i< str.length; i++) {
-
   // console.log(str[i]);
 //Meets the criteria, but in the wrong way:
 //   var lastThing = str.length-1;
 //   console.log("here",str[lastThing]);
-
+// for (var i = 0; i< str.length; i++) {
 //   if (str.charAt(0,lastThing) ==="+") {
 //     return true;
 //   }
@@ -25,28 +23,21 @@ for (var i = 0; i< str.length; i++) {
 
     // console.log(str.match(/[a-z]/i));
     // console.log(str[i]);
-
+  for (var i = 0; i< str.length; i++) {
     if (str[i].match(/[a-z]/i)) {
       // console.log("Letter")
         if (str[i-1] === "+" && str[i+1] === "+") {
-          return true;
+           
         }
         else {
-           return   false;
+           return false;
         }
-    }
-    else {
-      // console.log("No.")
-    }
-
-  // if (str[i].match(/[a-z]/i) && str[i-1] && str[i+1] === "+"){
-  //   console.log(true);
-  // }
-  // else {
-  //   console.log(false);
-  // }
-  
-}
+    };
+    // else {
+    //   // console.log("No.")
+    // }
+  }
+return true;
 }
 
 
